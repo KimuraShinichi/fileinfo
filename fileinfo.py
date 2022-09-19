122898946262126abd8e9f0f95657d6bb03169ba
KimuraShinichi <kimura.shinichi@ieee.org>
2022-09-20 08:03:05 +0900

    Added counters for file entries.

--- fileinfo.py
+++ fileinfo.py
@@ -9,6 +9,7 @@ import pathlib
 import platform
 import stat
 import sys
+import time
 
 def only_for_windows():
     """Return if the platform.system() is 'Windows'."""
@@ -23,6 +24,8 @@ class FileInfo:
         """Initialize this instance."""
         self.parser, self.args = FileInfo._parse_args(argv)
         self.my_out = sys.stdout
+        self.counters = {}
+        self.to_count = True
 
     def __repr__(self) -> str:
         """Returns instantiating representation."""
@@ -39,7 +42,7 @@ class FileInfo:
                 This program lists following attributes for each files in FileEntries.
                 file stat attributes as like as Linux/UNIX ls -l command and SHA-256 hash.
                 (
-                    access-mode[file-type;user-mode(rwx);group-mode(rwx);world-mode(rwx)],
+                    access-mode[file-kind;user-mode(rwx);group-mode(rwx);world-mode(rwx)],
                     number of file-links,
                     user(uid):group(gid),
                     size in bytes,
@@ -61,9 +64,11 @@ class FileInfo:
         parser.add_argument('-r', '--recursive', action='store_true',
             help='process recursively into sub directories')
         parser.add_argument('-x', '--excludes',
-            help='process skipping the specified semicolon-separated sub-directories and its children. (e.g. -x ".git;.svn")')
+            help='process skipping the specified '
+            'semicolon-separated sub-directories and its children. (e.g. -x ".git;.svn")')
         parser.add_argument('-o', '--output',
-            help='output file information list into the specified file. (e.g. `-o fileinfo.out\' or `--output /tmp/out.txt\')')
+            help='output file information list into the specified file. '
+            '(e.g. `-o fileinfo.out\' or `--output /tmp/out.txt\')')
         parser.add_argument('-f', '--force', action='store_true',
             help='force to output even if the output file is exists')
         args = vars(parser.parse_args(argv[1:]))
@@ -102,10 +107,10 @@ class FileInfo:
     @classmethod
     def _version(cls):
         """Returns the version message."""
-        return '1.0.11 (2022/09/19) for Python 3.x or later; '\
+        return '1.0.12 (2022/09/19) for Python 3.x or later; '\
             + '(Tested for Python 3.6.8 on Redhat Enterprise Linux 8.2, '\
             + 'Python 3.7.4 on Windows 10 Pro 21H1 and for Python 3.9.1 on MacBook Pro; '\
-            + 'Refine arg spec and format for file modified-time.)'
+            + 'Added counters for file entries.)'
 
     @classmethod
     def _copyright(cls):
@@ -121,6 +126,10 @@ class FileInfo:
     def _apache_licence_url(cls):
         """Returns the URL of The Apache Software Licence 2.0."""
         return 'https://www.apache.org/licenses/LICENSE-2.0'
+
+    def _info(self, *message):
+        concatenated = ' '.join(message)
+        print(f'INFO: {concatenated}', file=sys.stderr)
 
     def _warn(self, *message):
         concatenated = ' '.join(message)
@@ -164,6 +173,8 @@ class FileInfo:
             self._print_help(parser)
             return 1
 
+        self.counters = {}
+        start_time = time.time()
         output_file = args['output']
         ret = 0
         if output_file is None:
@@ -182,14 +193,21 @@ class FileInfo:
                 ret = 2
         else:
             ret = self._try_to_show(files, output_file=output_file)
+        if ret == 0:
+            end_time = time.time()
+            delta = end_time - start_time
+            self._info(f'FileEntries:')
+            n_file_entries = 0
+            for key in self.counters:
+                n_file_entries += self.counters[key]
+                self._info(f'  {key}: {self.counters[key]}')
+            self._info(f'  Total: {n_file_entries}')
+            if n_file_entries == 0:
+                per = '-'
+            else:
+                per = str(delta / n_file_entries)
+            self._info(f'Time: {delta}[sec] (Time/FileEntry: {per}[sec])')
         return ret
-
-    def _show_all(self, files, out):
-        self.my_out = out
-        names = {'unames':{}, 'gnames':{}}
-        for file in files:
-            self._dir_stat_invisibly(file, names)
-            self._show(file, names)
 
     def _try_to_show(self, files, output_file):
         if output_file is None:
@@ -201,15 +219,29 @@ class FileInfo:
                 self._show_all(files, my_out)
             return 0
         except IOError as io_error:
-            self._error(f'Failed to write: {output_file}: {io_error}')
+            self._error(f'Failed to write the output file: {output_file}: {io_error}')
             return 2
         finally:
             self.my_out = sys.stdout
+
+    def _show_all(self, files, out):
+        self.my_out = out
+        names = {'unames':{}, 'gnames':{}}
+        for file in files:
+
+            self.to_count = False
+            self._dir_stat_invisibly(file, names)
+            self.to_count = True
+
+            self._show(file, names)
 
     def _dir_stat_invisibly(self, file, names):
         """
         Make the directory of the file or the current directory
         without showing fileinfo.
+
+        In a case, a file ownership information is in the parent directory.
+        So we stat the parent directory before its children at least for top FileEntris.
         """
         a_directory = os.path.dirname(file)
         if a_directory:
@@ -251,8 +283,7 @@ class FileInfo:
         """Put a file-info string to the standard out."""
         print(fileinfo_str, file=self.my_out)
 
-    @staticmethod
-    def _stat_str(file, names):
+    def _stat_str(self, file, names):
         """
         Get a text representation of the stat values of the file
         using dictionary pair names.
@@ -272,7 +303,7 @@ class FileInfo:
             'gid': stat_info.st_gid,
             'gname': FileInfo._get_gname(stat_info.st_gid, path, names['gnames']),
             'size': stat_info.st_size,
-            'hash': FileInfo._get_hash(file, mode),
+            'hash': self._get_hash(file, mode),
             'mtime': FileInfo._as_datetime_style(stat_info.st_mtime),
             'link': FileInfo._get_link_symbol(path)
         }
@@ -353,10 +384,9 @@ class FileInfo:
                 xnames[xid] = xname
         return xname
 
-    @staticmethod
-    def _get_hash(file, mode):
+    def _get_hash(self, file, mode):
         """Get a hash value of the file on its mode."""
-        return f'SHA256:{FileInfo._sha256(file, mode)}'
+        return f'SHA256:{self._sha256(file, mode)}'
 
     @staticmethod
     def _get_link_symbol(path):
@@ -381,14 +411,19 @@ class FileInfo:
         """Returns the datetime of the timestamp."""
         return datetime.datetime.fromtimestamp(timestamp)
 
-    @staticmethod
-    def _sha256(file, mode):
+    def _sha256(self, file, mode):
         """
         Returns SHA-256 hash value hexadecimal text
         or some other text representation of the file
         which depends on its mode.
         """
-        if mode[0] == '-':
+        kind = mode[0]
+        if self.to_count:
+            if kind in self.counters:
+                self.counters[kind] += 1
+            else:
+                self.counters[kind] = 1
+        if kind == '-':
             return FileInfo._sha256_hex(file)
         a_dict = {
             'd': 'directory',
@@ -398,15 +433,15 @@ class FileInfo:
             'C': 'Contiguous-data', # high performance ("contiguous data") file
             'l': 'symbolic-link',
             'M': 'Migrated', # off-line ("migrated") file (Cray DMF)
-            'n': 'network special', # (HP-UX)
+            'n': 'network-special', # (HP-UX)
             's': 'socket',
             'P': 'FIFO',
             'p': 'FIFO',
             '?': 'some-other-file-type'
         }
-        if mode[0] in a_dict:
-            return a_dict[mode[0]]
-        raise TypeError(f'{mode[0]}: Unknown mode prefix of file: {file}')
+        if kind in a_dict:
+            return a_dict[kind]
+        return f'Unknown-file-kind(mode-prefix=\'{kind}\')'
 
     @staticmethod
     def _sha256_hex(file):
